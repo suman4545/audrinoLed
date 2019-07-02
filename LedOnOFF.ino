52b671f0d8052018e46220141f2d61bbfccf27af
suman4545 <smn.bsws@gmail.com>
2019-07-02 21:13:22 +0530

    added a files

---
+++ LedOnOFF.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(8,OUTPUT);
+   pinMode(7,OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(8,HIGH);
+ digitalWrite(7,LOW);
+ delay(500);
+ digitalWrite(8,LOW);
+ digitalWrite(7,HIGH);
+ delay(500);
+}
